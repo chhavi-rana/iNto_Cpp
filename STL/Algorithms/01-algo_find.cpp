d8ecbfe85d2afded6084a5c5df7dc64ce06dd282
= <chhavirana83@gmail.com>
2023-01-22 08:25:24 +0530

    search algorithm find

---
+++ STL/Algorithms/01-algo_find.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+    int size = 8;
+    int arr[] = {1,2,3,4,5,6,7,8};
+
+    int key;
+    cin >> key;                  
+
+    // Search Algorithm
+    auto it = find(arr, arr + size, key);
+
+    int index = it - arr;       //  When you subtract two pointers, as long as they point into the same array, the result is the number of elements separating them.
+    
+    if(index == size){
+        cout << key <<" not found" << endl;
+    }else{
+        cout << "Found at index: " << index << endl;
+    }
+    // cout << arr << endl;
+    // cout << it << endl;
+
+    return 0;
+}
