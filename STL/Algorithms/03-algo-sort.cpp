a08b8d2cfe0f6c8016a3781a1799b21c196ad839
= <chhavirana83@gmail.com>
2023-01-22 19:11:43 +0530

    sorting algorithm sort

--- STL/Algorithms/03-algo-sort.cpp
+++ STL/Algorithms/03-algo-sort.cpp
@@ -9,6 +9,7 @@ Auxiliary Space: O(1)
 
 The sort function in C++ uses IntroSort internally with the complexity of N*log(N)
 Resources : https://www.geeksforgeeks.org/introsort-or-introspective-sort/
+            https://www.youtube.com/watch?v=3pvZhwp0U9w
 
 
 */
@@ -17,10 +18,16 @@ Resources : https://www.geeksforgeeks.org/introsort-or-introspective-sort/
 #include<algorithm>
 using namespace std;
 
+
+// default comparator function of sort will swap if returned false, otherwise if reutured true it won't swap.
+
 bool compare(int a, int b){
     return a > b;                                          //  6434 234 72 6 5 3 
  /*  return a < b;                                        //   3 5 6 72 234 6434 */
+
+ /* To remember jis order ma chahiy vaise hi likh do. agar decreasing toh pahli greater than dusri -> a > b */
 }
+
 
 int main(){
 
@@ -34,7 +41,8 @@ int main(){
     
     */ 
     
-    sort(arr, arr + n, compare);                     
+    sort(arr, arr + n, compare);      
+    /* INbuilt for decreasing order :   sort(arr, arr + n, greater<int>) */               
 
     for(int i = 0; i < n; i++){
         cout << arr[i] << " ";                              
