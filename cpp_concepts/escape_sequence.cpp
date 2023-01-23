8f6e5b4fcb654f33ab7f2a425c6774c560096f99
= <chhavirana83@gmail.com>
2023-01-23 09:23:21 +0530

    learning escape sequences

---
+++ cpp_concepts/escape_sequence.cpp
@@ -0,0 +1,25 @@
+/* 
+
+The backslash (\) escape character turns special characters into string characters:
+
+Escape          character	        Result-Description
+\'	                '	                 Single quote
+\"	                "	                 Double quote
+\\ or \b	        \	                 Backslash
+\n                                       New line 
+\t                                       Tab
+\?                  ?                    Question mark
+\0                                       Null character (Represents termination of string) 
+
+
+An escape sequence is a sequence of characters used in formatting the output and are not displayed while printing text on to the screen, each having its own specific function. 
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "h";
+    return 0;
+}
