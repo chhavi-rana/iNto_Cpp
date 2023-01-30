359ce94ad28ac4c196738fbd54a869f6423c00a6
= <chhavirana83@gmail.com>
2023-01-30 15:24:10 +0530

    list methods

--- STL/List/01-list_basics.cpp
+++ STL/List/01-list_basics.cpp
@@ -30,12 +30,34 @@ int main(){
  
     // inserting element at a particular position
     list<string>::iterator it = fruits.begin();
+    // list<string>::iterator it = fruits.begin() + 3; --> INcorrect since list underlying memory is not linear like vector.
+    // indexing is not allowed in linked list. 
+    /* 
+    To do that particular option :
+        list<string>::iterator it = fruits.begin();
+        it++;
+        it++;
+        it++;
+    */
+
 
     fruits.insert(it, "mangoes");                  // mangoes-->watermelon-->pineapple-->grapes-->banana-->apple-->
+
+    
+
+    fruits.insert(it, "guava");
+    it = fruits.end();
+    fruits.insert(it, "guava");
+
+    // mangoes-->guava-->watermelon-->pineapple-->grapes-->banana-->apple-->guava-->
+
+     /* removes all the occurences of the word. */
+    fruits.remove("guava");
 
     for(auto x: fruits){
         cout << x << "-->";
     }
+    // mangoes-->watermelon-->pineapple-->grapes-->banana-->apple-->
 
     cout << endl << fruits.size();                  // 6
 
